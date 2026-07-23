c87af87825d448cd53998715e20cb3d1a179f8c0
Mikki <mikkiwaskel@gmail.com>
2026-07-23 19:36:35 +0530

    Create portfolio.js

---
+++ js/portfolio.js
@@ -0,0 +1,553 @@
+const portfolio = {
+
+"2d":[
+
+{
+
+title:"Bird Animation",
+
+description:"Photoshop, After effect",
+
+media: [
+
+        {
+          type: "video",
+          src: "assets/portfolio/2d/a1/1 bird character idle animation.mp4"
+        },
+
+        {
+          type: "video",
+          src: "assets/portfolio/2d/a1/2 bird character feeling sleepy.mp4"
+        },
+
+        {
+          type: "video",
+          src: "assets/portfolio/2d/a1/3 bird character sleeping animation.mp4"
+        }
+
+]
+
+},
+
+{
+
+title:"Photoshop, Unity engine",
+
+description:"Movie Poster",
+
+media: [
+
+        {
+          type: "video",
+          src: "assets/portfolio/2d/a2/7 butterfly animation.mp4"
+        },
+
+        {
+          type: "video",
+          src: "assets/portfolio/2d/a2/8 charactor animation prototype.mp4"
+        }
+
+]
+
+},
+{
+
+title:"Animation Workflow",
+
+description:"Photoshop, After effect",
+
+media: [
+
+        {
+          type: "video",
+          src: "assets/portfolio/2d/a3/6 Animation work in after effect.mp4"
+        }
+
+]
+
+},
+{
+
+title:"Particle Destortion",
+
+description:"Movie Poster",
+
+media: [
+
+        {
+          type: "video",
+          src: "assets/portfolio/2d/a4/10 Particle Distortion 1.mp4"
+        },
+
+        {
+          type: "video",
+          src: "assets/portfolio/2d/a4/11 Particle distortion 2.mp4"
+        }
+
+]
+
+},
+{
+
+title:"Ganesh Digital Painting",
+
+description:"Photoshop, Pentab",
+
+media: [
+
+        {
+          type: "video",
+          src: "assets/portfolio/2d/a5/12 pentab working.mp4"
+        }
+
+]
+
+},
+{
+
+title:"Logo Design",
+
+description:"Photoshop",
+
+media: [
+
+        {
+          type: "image",
+          src: "assets/portfolio/2d/a6/16 logo alignment3.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/2d/a6/15 logo alignment2.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/2d/a6/14 logo alignment1.jpg"
+        }
+
+]
+
+},
+{
+
+title:"Character Animation",
+
+description:"Photoshop 4FPS",
+
+media: [
+
+        {
+          type: "video",
+          src: "assets/portfolio/2d/a7/4 frame(4fps) based animation.mp4"
+        }
+
+]
+
+}
+],
+
+
+
+"3d":[
+
+{
+
+title:"Wheel",
+
+description:"Autodesk Maya, Substance Painter, Photoshop, Corona",
+
+media:[
+    
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a1/c1.jpg"
+        },
+
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a1/c2.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a1/c3.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a1/c4.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a1/c5.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a1/c6.jpg"
+        }
+
+
+]
+
+},
+
+{
+
+title:"Cyberpunk Car",
+
+description:"Autodesk Maya, Substance Painter, Photoshop, Corona, VRay",
+
+media:[
+    
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a2/a1.jpg"
+        },
+
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a2/a2.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a2/a3.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a2/a4.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a2/a5.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a2/a6.jpg"
+        },
+
+
+]
+
+},
+{
+
+title:"Interior Design",
+
+description:"Autodesk 3DsMax, Substance Painter, Corona, VRay",
+
+media:[
+    
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a3/b1.jpg"
+        },
+
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a3/b2.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a3/b3.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a3/b4.jpg"
+        }
+
+]
+
+},
+
+{
+
+title:"Dark Interior Design",
+
+description:"Autodesk 3DsMax, Substance Painter, Corona",
+
+media:[
+    
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a4/1.png"
+        },
+
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a4/2.png"
+        }
+
+]
+
+},
+
+{
+
+title:"3D Scope",
+
+description:"Autodesk Maya, Substance Painter, Corona",
+
+media:[
+    
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a5/Scope_With_Mat1.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a5/Scope_With_Mat2.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a5/Scope_With_Mat3.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a5/Scope_With_Mat4.jpg"
+        }
+
+]
+
+},
+
+{
+
+title:"M200 Sniper",
+
+description:"Autodesk Maya",
+
+media:[
+    
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a6/11.jpg"
+        },
+
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a6/12.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a6/13.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a6/14.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a6/15.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a6/16.jpg"
+        }
+
+
+]
+
+},
+{
+
+title:"Character Design",
+
+description:"Autodesk Maya, ZBrush, Photoshop",
+
+media:[
+    
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a7/Portfolio_Gallery_S2A.jpg"
+        },
+
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a7/Portfolio_Gallery_S2B.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a7/Portfolio_Gallery_S2C.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a7/Portfolio_Gallery_S2D.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a7/Portfolio_Gallery_S2E.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a7/Portfolio_Gallery_S2F.jpg"
+        },
+        {
+          type: "image",
+          src: "assets/portfolio/3d/a7/Portfolio_Gallery_S2G.jpg"
+        }
+
+
+]
+
+}
+
+],
+
+
+
+gameplay:[
+
+{
+
+title:"Sliding Mechanics",
+
+description:"Unity Engine, Photoshop",
+
+media:[
+    
+        {
+          type: "video",
+          src: "assets/portfolio/gameplay/a1/5_SlidingWithPhysicsCollision.mp4"
+        }
+
+
+]
+
+},
+
+{
+
+title:"Hotel Management Mechanics",
+
+description:"Unity Engine",
+
+media:[
+    
+        {
+          type: "video",
+          src: "assets/portfolio/gameplay/a2/6_3dNavMeshAiMovements.mp4"
+        }
+
+
+]
+
+},
+{
+
+title:"VR Acitvity 1",
+
+description:"Unity Engine, Photoshop",
+
+media:[
+    
+        {
+          type: "video",
+          src: "assets/portfolio/gameplay/a3/1_Vr3d_GamePlay1.mp4"
+        }
+
+
+]
+
+},
+{
+
+title:"VR Activity 2",
+
+description:"Gameplay",
+
+media:[
+    
+        {
+          type: "video",
+          src: "assets/portfolio/gameplay/a4/2_Vr3d_Gameplay2.mp4"
+        }
+
+
+]
+
+},
+{
+
+title:"Multiplayer Synchronize",
+
+description:"Unity Egnine",
+
+media:[
+    
+        {
+          type: "video",
+          src: "assets/portfolio/gameplay/a5/3_3dMultiplayerSynchronise.mp4"
+        }
+
+
+]
+
+},
+{
+
+title:"Local Simulation & Weapon System",
+
+description:"Unity Engine",
+
+media:[
+    
+        {
+          type: "video",
+          src: "assets/portfolio/gameplay/a6/9_3dWeaponSystem.mp4"
+        }
+
+
+]
+
+},
+{
+
+title:"Laser Dodger",
+
+description:"Unity Engine",
+
+media:[
+    
+        {
+          type: "video",
+          src: "assets/portfolio/gameplay/a7/7_3dLaserDodger_GamePlay.mp4"
+        }
+
+
+]
+
+},
+{
+
+title:"Physics 3D",
+
+description:"Unity Engine",
+
+media:[
+    
+        {
+          type: "video",
+          src: "assets/portfolio/gameplay/a8/4_3dPhysics.mp4"
+        }
+
+
+]
+
+},
+{
+
+title:"Elevator Mechanics",
+
+description:"Unity Engine, Photoshop",
+
+media:[
+    
+        {
+          type: "video",
+          src: "assets/portfolio/gameplay/a9/Final_resultCompressed.mp4"
+        }
+
+
+]
+
+}
+]
+
+};
